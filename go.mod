cad2bc4b8a6fcbe0fc7d8ffbbca5ca5f0f4f42db
Greg Magarshak <greg@qbix.com>
2026-03-15 22:40:12 -0400

    Support broader RFC 8785 JSON Canonicalization

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/OpenClaiming/Go
+
+go 1.21
+
+require github.com/gowebpki/jcs v1.0.0
--- openclaiming.go
+++ openclaiming.go
@@ -2,22 +2,150 @@ package openclaiming
 
 import (
 	"crypto/ecdsa"
+	"crypto/rand"
 	"crypto/sha256"
+	"encoding/asn1"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
+	"math/big"
+	"sort"
+
+	jcs "github.com/gowebpki/jcs"
 )
 
-type OpenClaim struct {}
-
-func Canonicalize(claim map[string]interface{}) ([]byte,error) {
-	delete(claim,"sig")
-	return json.Marshal(claim)
+type ecdsaSignature struct {
+	R, S *big.Int
 }
 
-func Sign(claim map[string]interface{}, priv *ecdsa.PrivateKey) (string,error) {
-	canon,_ := Canonicalize(claim)
+func normalize(v interface{}) interface{} {
+
+	switch t := v.(type) {
+
+	case map[string]interface{}:
+
+		keys := make([]string, 0, len(t))
+		for k := range t {
+			keys = append(keys, k)
+		}
+
+		sort.Strings(keys)
+
+		out := map[string]interface{}{}
+
+		for _, k := range keys {
+			out[k] = normalize(t[k])
+		}
+
+		return out
+
+	case []interface{}:
+
+		arr := make([]interface{}, len(t))
+
+		for i, v := range t {
+			arr[i] = normalize(v)
+		}
+
+		return arr
+	}
+
+	return v
+}
+
+func fallbackCanonicalize(claim map[string]interface{}) ([]byte, error) {
+
+	obj := map[string]interface{}{}
+
+	for k, v := range claim {
+		if k != "sig" {
+			obj[k] = v
+		}
+	}
+
+	return json.Marshal(normalize(obj))
+}
+
+func Canonicalize(claim map[string]interface{}) ([]byte, error) {
+
+	obj := map[string]interface{}{}
+
+	for k, v := range claim {
+		if k != "sig" {
+			obj[k] = v
+		}
+	}
+
+	raw, err := json.Marshal(obj)
+	if err != nil {
+		return nil, err
+	}
+
+	// Try strict RFC8785 canonicalization
+	canon, err := jcs.Transform(raw)
+	if err == nil {
+		return canon, nil
+	}
+
+	// Fallback deterministic canonicalization
+	return fallbackCanonicalize(claim)
+}
+
+func Sign(claim map[string]interface{}, priv *ecdsa.PrivateKey) (map[string]interface{}, error) {
+
+	canon, err := Canonicalize(claim)
+	if err != nil {
+		return nil, err
+	}
+
 	hash := sha256.Sum256(canon)
-	r,s,_ := ecdsa.Sign(nil,priv,hash[:])
-	sig := append(r.Bytes(), s.Bytes()...)
-	return base64.StdEncoding.EncodeToString(sig),nil
+
+	r, s, err := ecdsa.Sign(rand.Reader, priv, hash[:])
+	if err != nil {
+		return nil, err
+	}
+
+	sig, err := asn1.Marshal(ecdsaSignature{r, s})
+	if err != nil {
+		return nil, err
+	}
+
+	out := map[string]interface{}{}
+	for k, v := range claim {
+		out[k] = v
+	}
+
+	out["sig"] = base64.StdEncoding.EncodeToString(sig)
+
+	return out, nil
+}
+
+func Verify(claim map[string]interface{}, pub *ecdsa.PublicKey) (bool, error) {
+
+	sigB64, ok := claim["sig"].(string)
+	if !ok {
+		return false, errors.New("missing signature")
+	}
+
+	sig, err := base64.StdEncoding.DecodeString(sigB64)
+	if err != nil {
+		return false, err
+	}
+
+	var esig ecdsaSignature
+	_, err = asn1.Unmarshal(sig, &esig)
+	if err != nil {
+		return false, err
+	}
+
+	canon, err := Canonicalize(claim)
+	if err != nil {
+		return false, err
+	}
+
+	hash := sha256.Sum256(canon)
+
+	ok = ecdsa.Verify(pub, hash[:], esig.R, esig.S)
+
+	return ok, nil
 }
